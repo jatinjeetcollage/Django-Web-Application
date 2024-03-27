0ee8fa92036086936b038b436967776dd4bb5f95
jatinjeetcollage <70069514+jatinjeet@users.noreply.github.com>
2024-03-27 14:20:45 -0400

    Working on the Post Detail Page Complete

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,8 @@ from .views import (
     post_list_and_create,
     load_post_data_view,
     like_unlike_post,
-    post_detail
+    post_detail,
+    post_detail_data_view
    
 )
 
@@ -14,5 +15,6 @@ urlpatterns = [
     path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('<pk>/', post_detail, name='post-detail'),
     path('data/<int:num_posts>/', load_post_data_view,name='posts-data'),
+    path('<pk>/data/',post_detail_data_view,name='post-detail-data'),
     
 ]
