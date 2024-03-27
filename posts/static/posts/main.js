567813a2e4a0e6d0bac6685fcaa913c269761cf9
jatinjeetcollage <70069514+jatinjeet@users.noreply.github.com>
2024-03-27 12:44:55 -0400

    Creating the Post Detail Page Complete

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -10,6 +10,9 @@ const title = document.getElementById('id_title')
 const body = document.getElementById('id_body')
 const csrf = document.getElementsByName('csrfmiddlewaretoken')
 const alertBox = document.getElementById('alert-box')
+const url = window.location.href
+
+
 console.log('csrf', csrf[0].value)
 
 const getCookie = (name) => {
@@ -77,7 +80,7 @@ const getData = () => {
                             <div class="card-footer">
                                 <div class ="row">
                                     <div class="col-2">
-                                        <a href="#" class="btn btn-primary">Details</a>
+                                        <a href="${url}${element.id}" class="btn btn-primary">Details</a>
                                     </div>
                                     <div class="col-2">
                                         <form class="like-unlike-forms" data-form-id="${element.id}">
